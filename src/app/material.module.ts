5dd9cd49cfb7764a8242177479af9cca7731e493
ahmedsayed621 <ahmedsayed621@yahoo.com>
2023-07-18 06:31:06 +0200

    Angular Material and structure of the project

---
+++ src/app/material.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from "@angular/core";
+import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input'
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from "@angular/material/core";
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+@NgModule({
+  imports :[
+      MatIconModule,
+      MatButtonModule,
+      MatFormFieldModule,
+      MatInputModule,
+      MatDatepickerModule,
+      MatNativeDateModule,
+      MatCheckboxModule
+  ],
+  exports:[
+  MatIconModule,
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatCheckboxModule
+]
+})
+export class MaterialModule{}
+
